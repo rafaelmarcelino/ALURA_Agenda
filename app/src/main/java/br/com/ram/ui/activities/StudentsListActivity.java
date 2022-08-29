aaf67d9dfd85071ad976f6de761e2ee7ae96891c
RMarcelino <r.marcelino@ram-automation.com.br>
2022-08-29 10:24:55 -0300

    Refactor to show a list of students instead string.

--- app/src/main/java/br/com/ram/ui/activities/StudentsListActivity.java
+++ app/src/main/java/br/com/ram/ui/activities/StudentsListActivity.java
@@ -12,6 +12,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import br.com.ram.R;
+import br.com.ram.model.Student;
+import br.com.ram.model.StudentDAO;
 
 public class StudentsListActivity extends AppCompatActivity {
     @Override
@@ -21,12 +23,13 @@ public class StudentsListActivity extends AppCompatActivity {
         setTitle("Students list");
         //Call a view in this activity
         setContentView(R.layout.lista_alunos_activity);
+
         //Creating students
-        List <String> students = new ArrayList<>(Arrays.asList("Rafael","Maria","Carmem"));
+        List <Student> students = new StudentDAO().getStudents();
         //Searching List View
         ListView lv_students = findViewById(R.id.lista_alunos_activity_lv_students);
         //Creating an adapter to send data to List View
-        ArrayAdapter<String> adapter_lv_students = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,students);
+        ArrayAdapter<Student> adapter_lv_students = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,students);
         //Linking the adapter in list view
         lv_students.setAdapter(adapter_lv_students);
      }
