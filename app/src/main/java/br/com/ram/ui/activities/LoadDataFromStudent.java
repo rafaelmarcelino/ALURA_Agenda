df985c19976f741a0e7ed9aaf2424ee153fc6fff
RMarcelino <r.marcelino@ram-automation.com.br>
2022-08-29 13:03:35 -0300

    Added activity and layout to show student data.

---
+++ app/src/main/java/br/com/ram/ui/activities/LoadDataFromStudent.java
@@ -0,0 +1,16 @@
+package br.com.ram.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ram.R;
+
+public class LoadDataFromStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_data_from_student);
+    }
+}
