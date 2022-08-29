0b52f955e5e3155451e5e2daec5fa8313b393d9b
RMarcelino <r.marcelino@ram-automation.com.br>
2022-08-29 08:01:30 -0300

    Refactoring code.

--- app/src/main/java/br/com/ram/ListaDeAlunosActivity.java
+++ app/src/main/java/br/com/ram/ListaDeAlunosActivity.java
@@ -11,18 +11,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaDeAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Setting the title os app bar
         setTitle("Students list");
         //Call a view in this activity
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.lista_alunos_activity);
         //Creating students
         List <String> students = new ArrayList<>(Arrays.asList("Rafael","Maria","Carmem"));
         //Searching List View
-        ListView lv_students = findViewById(R.id.activity_main_lv_students);
+        ListView lv_students = findViewById(R.id.lista_alunos_activity_lv_students);
         //Creating an adapter to send data to List View
         ArrayAdapter<String> adapter_lv_students = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,students);
         //Linking the adapter in list view
