428b584de240972c1ec022e4b19bcb170121e21b
RMarcelino <r.marcelino@ram-automation.com.br>
2022-09-01 06:24:36 -0300

    Created class to constants.

---
+++ app/src/main/java/br/com/ram/tools/Constants.java
@@ -0,0 +1,9 @@
+package br.com.ram;
+
+public abstract class Constants {
+
+    public static final int START_INDEX = 0;
+    public static final int EMPTY = 0;
+    public static final int DEFAULT_ERROR_INDEX = -1;
+    public static final int WRONG_INDEX = -1;
+}
