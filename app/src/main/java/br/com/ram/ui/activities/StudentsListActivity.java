235cc7d42ff99d21e92cb435a70810ced67467cb
RMarcelino <r.marcelino@ram-automation.com.br>
2022-09-03 15:38:16 -0300

    Refactored class to fit better with contex.

--- app/src/main/java/br/com/ram/ui/activities/StudentsListActivity.java
+++ app/src/main/java/br/com/ram/ui/activities/StudentsListActivity.java
@@ -24,6 +24,10 @@ public class StudentsListActivity extends AppCompatActivity {
     //Creating variables of views
     private FloatingActionButton fabAddStudent;
     private ListView lv_students;
+    //Creating an adapter to send data to List View
+    ArrayAdapter<Student> adapter_lv_students;
+    //Creating tool to handle students
+    StudentDAO studentDAO;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,13 +45,11 @@ public class StudentsListActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //Load all students stored
-        final List<Student> allStudentsStored = getAllStudentsStored();
-        //Set adapters to show all students stored
-        setAdapters(allStudentsStored);
+        //Update adapters to show all students stored
+        updatingDataOfAdapter(studentDAO.getStudents());
         //Showing how many students we have created if greater than 0
-        if (allStudentsStored.size() > Constants.EMPTY){
-            Toast.makeText(this, "Quantity of students = "+ allStudentsStored.size(), Toast.LENGTH_SHORT).show();
+        if (studentDAO.getStudents().size() > Constants.EMPTY){
+            Toast.makeText(this, "Quantity of students = "+ studentDAO.getStudents().size(), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -55,6 +57,12 @@ public class StudentsListActivity extends AppCompatActivity {
     private void initVariables() {
         fabAddStudent = findViewById(R.id.activity_students_list_fab_add_students);
         lv_students = findViewById(R.id.activity_students_list_lv_students);
+
+        adapter_lv_students = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        //Linking the adapter in list view
+        lv_students.setAdapter(adapter_lv_students);
+
+        studentDAO = new StudentDAO();
     }
     private void callListenersOfViews(){
         //Floating action button
@@ -89,11 +97,11 @@ public class StudentsListActivity extends AppCompatActivity {
         //Creating students
         return new StudentDAO().getStudents();
     }
-    private void setAdapters(List<Student>students){
-        //Creating an adapter to send data to List View
-        ArrayAdapter<Student> adapter_lv_students = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,students);
-        //Linking the adapter in list view
-        lv_students.setAdapter(adapter_lv_students);
+    private void updatingDataOfAdapter(List<Student>students){
+        //Clear previos data of adapter
+        adapter_lv_students.clear();
+        //Updating data of adapter
+        adapter_lv_students.addAll(students);
     }
     private void openActivityToFillDataOfStudent() {
         startActivity(new Intent(StudentsListActivity.this,FormStudentActivity.class));
