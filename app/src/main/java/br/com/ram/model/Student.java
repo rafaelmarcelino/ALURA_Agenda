790ed8c259c380c245056b6e4898af52539f7c8d
RMarcelino <r.marcelino@ram-automation.com.br>
2022-09-03 07:35:32 -0300

    Added parameter id in class Student.

--- app/src/main/java/br/com/ram/model/Student.java
+++ app/src/main/java/br/com/ram/model/Student.java
@@ -3,18 +3,26 @@ package br.com.ram.model;
 import java.io.Serializable;
 
 public class Student implements Serializable {
+    private long id;
     private String name;
     private String phone;
     private String email;
 
     //Constructor
     public Student(String name, String phone, String email) {
+        this.id ++;
         this.name = name;
         this.phone = phone;
         this.email = email;
     }
 
     //Getters
+
+
+    public long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
