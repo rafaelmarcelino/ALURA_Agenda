391a70ff7c9234388e75012fdcba174a808cc94a
RMarcelino <r.marcelino@ram-automation.com.br>
2022-09-20 22:39:32 -0300

    Created test class to learn about constraint layout.

---
+++ app/src/main/java/br/com/ram/ui/activities/TestConstraintActivity.java
@@ -0,0 +1,16 @@
+package br.com.ram.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ram.R;
+
+public class TestConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_constraint);
+    }
+}
