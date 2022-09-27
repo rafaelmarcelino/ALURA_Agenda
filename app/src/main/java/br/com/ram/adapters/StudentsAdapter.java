5b3ef755734faeedf1caf0d11a1f170900705877
RMarcelino <r.marcelino@ram-automation.com.br>
2022-09-27 14:00:52 -0300

    Updated adapter to handle new parameters of student object.

--- app/src/main/java/br/com/ram/adapters/StudentsAdapter.java
+++ app/src/main/java/br/com/ram/adapters/StudentsAdapter.java
@@ -1,10 +1,13 @@
 package br.com.ram.adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,6 +15,7 @@ import java.util.List;
 
 import br.com.ram.R;
 import br.com.ram.model.Student;
+import br.com.ram.tools.Constants;
 
 public class StudentsAdapter extends BaseAdapter {
     private final Context context;
@@ -50,11 +54,29 @@ public class StudentsAdapter extends BaseAdapter {
 
     private void fillDataInViews(int position, View viewInflated) {
         //Get views
-        final TextView tvName = viewInflated.findViewById(R.id.lv_item_student_name);
-        final TextView tvPhone = viewInflated.findViewById(R.id.lv_item_student_phone);
+        final TextView tvName = viewInflated.findViewById(R.id.cell_constraint_tv_name_student);//(R.id.lv_item_student_name);
+        final TextView tvPhone = viewInflated.findViewById(R.id.cell_constraint_tv_phone_student);//(R.id.lv_item_student_phone);
+        final TextView tvEmail = viewInflated.findViewById(R.id.cell_constraint_tv_email_student);
+        final TextView tvAge = viewInflated.findViewById(R.id.cell_constraint_tv_age_student);
+        final ImageView ivPhoto = viewInflated.findViewById(R.id.cell_constraint_iv_photo_student);
+        final View vDivider = viewInflated.findViewById(R.id.cell_constraint_view_divider);
+
+         //Data based on gender
+        final Resources resources = context.getResources();
+        if (this.students.get(position).getGender()== Constants.MALE) {
+            ivPhoto.setImageDrawable(context.getDrawable(R.drawable.male_avatar));
+            vDivider.setBackgroundColor(resources.getColor(R.color.blue_200));
+        }else{
+            ivPhoto.setImageDrawable(context.getDrawable(R.drawable.female_avatar));
+            vDivider.setBackgroundColor(resources.getColor(R.color.purple_200));
+        }
+
         //Fill data
         tvName.setText(this.students.get(position).getName());
+        tvEmail.setText(this.students.get(position).getEmail());
         tvPhone.setText(this.students.get(position).getPhone());
+        tvAge.setText(this.students.get(position).getAge() + " yrs");
+
     }
 
     public void updateDataFromList(List<Student> students){
